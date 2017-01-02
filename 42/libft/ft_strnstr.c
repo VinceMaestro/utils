71a6ea9f01e84cfb6250051c2606e15a6c7ff2d3
Vincent PETIT <vpetit@e3r4p20.42.fr>
2017-01-02 23:19:03 +0100

    libft

--- 42/libft/ft_strnstr.c
+++ 42/libft/ft_strnstr.c
@@ -6,12 +6,11 @@
 /*   By: vpetit <vpetit@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/21 18:54:16 by vpetit            #+#    #+#             */
-/*   Updated: 2017/01/02 21:44:02 by vpetit           ###   ########.fr       */
+/*   Updated: 2017/01/02 22:18:44 by vpetit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
@@ -26,7 +25,7 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 		len_little = ft_strlen(little);
 		if (!little)
 			return ((char*)big);
-		if (len_big >= len_little)
+		if (len_big >= len_little && len >= len_little)
 		{
 			while (i <= len_big - len_little && i <= len - len_little)
 			{
@@ -38,14 +37,3 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	}
 	return (NULL);
 }
-//
-// int	main(void)
-// {
-// 	char	*str;
-//
-// 	str = (char*)malloc(sizeof(char) * 4);
-//
-// 	if (strnstr(str, "Hello", 3) != ft_strnstr(str, "Hello", 3))
-// 		printf("Should BE :  %s  IS :  %s  \n", strnstr(str, "Hello", 3), ft_strnstr(str, "Hello", 3));
-// 	else printf("GG \n");
-// }
