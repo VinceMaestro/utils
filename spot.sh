6a5efcf68aecdfa375f6bf56ee09dee2c648966a
VinceMaestro <vpetit@e3r5p17.42.fr>
2017-08-30 19:54:57 +0200

    update

--- spot.sh
+++ spot.sh
@@ -1,3 +1,5 @@
-read -p 'Entrez votre login : ' login;
-read -p 'Interval entre 2 mesures: ' tmp;
-for i in `seq 1 36000` ; do who | grep -v $login; ps -ax | grep ssh | grep -v "grep ssh" ; sleep $tmp ;done ;
+# read -p 'Entrez votre login : ' login;
+# read -p 'Interval entre 2 mesures: ' tmp;
+# for i in `seq 1 36000` ; do who | grep -v $login; ps -ax | grep ssh | grep -v "grep ssh" ; sleep $tmp ;done ;
+
+for i in `seq 1 36000` ; do who | grep -v $USER; ps -ax | grep ssh | grep -v "grep ssh" ; sleep 5 ;done ;
